88a9ba11ad40efaae43d844315835c1044efb4ab
albedohuang <albedohuang@tencent.com>
2026-04-13 19:28:50 +0800

    feat: 新增 mall-app-web 前端项目

---
+++ mall-app-web/postcss.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+  parser: require('postcss-comment'),
+  plugins: [
+    require('autoprefixer')({
+      overrideBrowserslist: ["Android >= 4.4", "ios >= 9"]
+    })
+  ]
+}
